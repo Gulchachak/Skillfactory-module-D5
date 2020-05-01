e3b82dda63082cabc65dd8ab9b6f9d7991c8f005
Gulchachak <ilkaeva85@rambler.ru>
2020-05-02 00:03:11 +0500

    fixing

--- my_site/settings.py
+++ my_site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY_DJANGO = 'ojf+-3b4vs!a^l=8uv9_$bzrtmljt=u+qcg$9!lmx5#4-y$wqe'
-SECRET_KEY_DJANGO = os.environ.get('SECRET_KEY_DJANGO')
+SECRET_KEY = os.environ.get('SECRET_KEY_DJANGO')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
